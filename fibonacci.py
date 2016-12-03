e0398ac7c13a887b6c2c11d7c92a8a8aefad7b2d
FreeShany <alexandra.dremina@gmail.com>
2016-12-03 18:34:33 +0000

    Algorithm was adjusted for python 3

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,9 @@
 def fibonachi(n):
     fib_n = [0, 1]
-    for _ in range(2, n + 1):
+    for i in range(2, n + 1):
         fib_n[0], fib_n[1] = fib_n[1], fib_n[0] + fib_n[1]
     return fib_n[1]
 
 
-n = input("Type a real number from 1 to 40: ")
-fib_n = fibonachi(n)
-print "Answer for n = %d is %d" % (n, fib_n)
+n = int(input())
+print(fibonachi(n))
